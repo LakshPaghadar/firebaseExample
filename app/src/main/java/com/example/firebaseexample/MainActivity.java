1a8c0fdea5f8af25f860f43a83e6bd06ec0d68b8
laksh patel <lakshy1662001@gmail.com>
2021-11-15 14:47:22 +0530

    firstDemo

---
+++ app/src/main/java/com/example/firebaseexample/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.firebaseexample;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //code for store data
+
+        //create firebase instance and get that inctance to send data in firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        //create firebase dataset referance which will store your data in 'massage' referance.
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+        // code for retrive data
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Toast.makeText(getApplicationContext(), value, Toast.LENGTH_SHORT).show();
+                //Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                //Log.w(TAG, "Failed to read value.", error.toException());
+
+            }
+        });
+    }
+}
